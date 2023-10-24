788094f35a3ba33aa5e10d29b4185abcea8fb9f1
sparisca04 <simonpariscam@gmail.com>
2023-10-23 21:56:41 -0500

    empiezo navegacion

---
+++ main/static/script.js
@@ -0,0 +1,4 @@
+function toggleMenu(){
+    var navLinks = document.getElementById('nav-items');
+    navLinks.classList.toggle('navbar-nav-active');
+}
---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"main/home.html")
+
+def proyectos(request):
+    return render(request, "main/proyectos.html")
+
+def nosotros(request):
+    return render(request, "main/nosotros.html")
