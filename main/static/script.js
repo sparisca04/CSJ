89e5b3b38efe7b134c730ade8a86877a26190797
[Simon Parisca <simonpariscam@gmail.com>
2023-10-25 09:07:37 -0500

    por fin

--- main/static/script.js
+++ main/static/script.js
@@ -2,6 +2,18 @@ function toggleMenu(){
     var navLinks = document.getElementById('nav-items');
     navLinks.classList.toggle('navbar-nav-active');
 }
+window.onscroll = function(){
+  var logo = document.getElementById('logo');
+  var logoSmall = document.getElementById('logo-pequeño');
+  if(window.scrollY >= 50){
+    logo.classList.add('hidden');
+    logoSmall.classList.remove('hidden');
+  } else {
+    logo.classList.remove('hidden');
+    logoSmall.classList.add('hidden');
+  }
+}
+
 function isVisible(element) {
     var rect = element.getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
