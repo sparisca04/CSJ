d89bbead8ec35c8bf8c5fc035a022bab6c3791db
NicolasEche <nicolas.echeverri41@eia.edu.co>
2023-10-25 07:51:51 -0500

    borre cositas

--- main/static/script.js
+++ main/static/script.js
@@ -2,18 +2,3 @@ function toggleMenu(){
     var navLinks = document.getElementById('nav-items');
     navLinks.classList.toggle('navbar-nav-active');
 }
-function isVisible(element) {
-    var rect = element.getBoundingClientRect();
-    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-  }
-  
-  var contador = document.getElementById("contador");
-  
-  window.addEventListener("scroll", function() {
-    if (isVisible(contador)) {
-      contador.classList.add("animar");
-    } else {
-      contador.classList.remove("animar");
-    }
-  });
