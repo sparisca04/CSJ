b23217b52cd06f03bb0633a04b0d78e1db31860d
sparisca04 <simonpariscam@gmail.com>
2023-12-15 15:00:14 -0500

    ya funciona el dropdown de proyectos

--- main/views.py
+++ main/views.py
@@ -3,24 +3,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import Proyecto, ProyectoDestacado
 
 # Create your views here.
-def nav(request):
-    proyectos = Proyecto.objects.all()
-    return render(request, "main/nav.html", {'proyectos': proyectos})
 
 def home(request):
-    proyectos = ProyectoDestacado.objects.all()
-    return render(request, "main/home.html", {'proyectos': proyectos})
+    proyectos = Proyecto.objects.all()
+    proyectosDestacados = ProyectoDestacado.objects.all().order_by('nombre')
+    return render(request, "main/home.html", {'proyectosDestacados': proyectosDestacados, 'proyectos': proyectos})
 
 def proyectos(request):
     proyectos = Proyecto.objects.all()
     return render(request, "main/proyectos.html", {'proyectos': proyectos})
 
 def nosotros(request):
-    return render(request, "main/nosotros.html")
+    proyectos = Proyecto.objects.all()
+    return render(request, "main/nosotros.html", {'proyectos': proyectos})
 
 def contact(request):
-    return render(request, "main/contact.html")
+    proyectos = Proyecto.objects.all()
+    return render(request, "main/contact.html", {'proyectos': proyectos})
 
 def proyecto(request, id):
+    proyectos = Proyecto.objects.all()
     proyecto = get_object_or_404(Proyecto, id=id)
-    return render(request, "main/proyecto.html", {'proyecto':proyecto})
+    return render(request, "main/proyecto.html", {'proyecto':proyecto, 'proyectos': proyectos})
